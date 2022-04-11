51ee2f7c6fe7bf3ca468879dd451e0eded1f6266
Rukosa <mwmcginnis490@gmail.com>
2022-04-11 12:44:17 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-InstaFilter/MICHAELMCGINNIS_InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_InstaFilterApp.swift
+//  MICHAELMCGINNIS-InstaFilter
+//
+//  Created by Michael Mcginnis on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

7219d7b55707abb91e28e9400475b97be94175c2
Rukosa <mwmcginnis490@gmail.com>
2022-04-11 15:16:34 -0700

    Final project with all challenges. Added kaleidoscope, monochrome, and bloom!

--- MICHAELMCGINNIS-InstaFilter/ContentView.swift
+++ MICHAELMCGINNIS-InstaFilter/ContentView.swift
@@ -6,11 +6,155 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
 
 struct ContentView: View {
+    @State private var image: Image?
+    
+    @State private var filterIntensity = 0.5
+    @State private var radiusIntensity = 0.5
+    @State private var scaleIntensity = 0.5
+    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilterSheet = false
+    @State private var processedImage: UIImage?
+    let context = CIContext()
+    
+    func applyProcessing(){
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey){
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey){
+            currentFilter.setValue(radiusIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey){
+            currentFilter.setValue(scaleIntensity * 10, forKey: kCIInputScaleKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else {return}
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+            processedImage = uiImage
+        }
+    }
+    
+    func setFilter(_ filter: CIFilter){
+        currentFilter = filter
+        loadImage()
+    }
+    
+    func save(){
+        guard let processedImage = processedImage else {
+            return
+        }
+        let imageSaver = ImageSaver()
+        
+        imageSaver.successHandler = {
+            print("Success!")
+        }
+        
+        imageSaver.errorHandler = {
+            print("Oops: \($0.localizedDescription)")
+        }
+        
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+    }
+    
+    func loadImage(){
+        guard let inputImage = inputImage else {
+            return
+        }
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                ZStack{
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+                
+                HStack{
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity){ _ in
+                            applyProcessing()
+                        }
+                }
+                .padding(.vertical)
+                HStack{
+                    Text("Radius")
+                    Slider(value: $radiusIntensity)
+                        .onChange(of: radiusIntensity){ _ in
+                            applyProcessing()
+                        }
+                }
+                HStack{
+                    Text("Scale")
+                    Slider(value: $scaleIntensity)
+                        .onChange(of: scaleIntensity) { _ in
+                            applyProcessing()
+                        }
+                }
+                
+                HStack{
+                    Button("Change Filter"){
+                        showingFilterSheet = true
+                    }
+                    Spacer()
+                    Button("Save", action: save)
+                        .disabled(image == nil)
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+        .navigationTitle("Instafilter")
+        .sheet(isPresented: $showingImagePicker){
+            ImagePicker(image: $inputImage)
+        }
+        .confirmationDialog("Select a filter", isPresented: $showingFilterSheet){
+            VStack{
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                //extra filters
+                Button("Kaleidoscope"){setFilter(CIFilter.kaleidoscope()) }
+                Button("Monochrome"){setFilter(CIFilter.colorMonochrome())}
+                VStack{
+                    Button("Bloom"){ setFilter(CIFilter.bloom())}
+                    Button("Cancel", role: .cancel) { }
+                }
+            }
+        }
+        .onChange(of: inputImage){ _ in
+            loadImage()
+        }
     }
 }
 
---
+++ MICHAELMCGINNIS-InstaFilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  MICHAELMCGINNIS-InstaFilter
+//
+//  Created by Michael Mcginnis on 4/11/22.
+//
+
+import Foundation
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveComplete), nil)
+    }
+
+    @objc func saveComplete(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
